e76995b137567caeccd2cb2383643fe280846328
mak20th <mak20th@gmail.com>
2019-03-21 13:08:11 -0500

    added state in App.js

---
+++ jizong-mohammad-trello-refactor/src/List.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Card from './Card'
+import './List.css';
+
+export default function List(props) {
+  console.log("inside list component", props)
+  return (
+    <section className='List'>
+      <header className='List-header'>
+        <h2>{props.list.header}</h2>
+      </header>
+      <div className='List-cards'>
+        {props.list.allcards.map((card) =>
+          <Card
+            key={card.id}
+            title={card.title}
+            content={card.content}
+          />
+        )}
+        <button
+          type='button'
+          className='List-add-button'
+        >
+          + Add Random Card
+        </button>
+      </div>
+    </section>
+  )
+}
